fa905c53f47f625712735857de517f02b90103d0
smurfedGitHubIO <clydefrongoso@gmail.com>
2023-02-27 07:34:02 +0800

    Login Logout + LocalStorage

---
+++ server/routes/userAPI.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/signup', (req, res) => {
+
+})
+
+router.post('/login', (req, res) => {
+  
+})
+
+router.patch('/updateProfile', (req, res) => {
+  
+})
+
+module.exports = router;
