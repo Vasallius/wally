4fb518b9f2791024435b085c3c80a4ed96def9c5
miellekc <jccalderon@up.edu.ph>
2023-02-28 22:36:59 +0800

    removed unnecessary

--- client/src/routes/login/+page.svelte
+++ client/src/routes/login/+page.svelte
@@ -19,28 +19,33 @@
     });
   }
 </script>
-<form>
-  <label>
-    Username:
-    <input type="text" bind:value={username}/>
-  </label>
-  <label>
-    Full Name:
-    <input type="text" bind:value={fullname} />
-  </label>
-  <label>
-    Email:
-    <input type="email" bind:value={email} />
-  </label>
-  <label>
-    Password:
-    <input type="password" bind:value={password} />
-  </label>
-  <button on:click={submit}>
-    Submit
+<div class="flex flex-col h-screen max-w-md m-auto border-solid border-2 border-sky-500 pt-8">
+  <button class="mx-7 w-12 h-12 bg-light-green mb-8 rounded-full">
+    <img class= "w-12 h-12" src="/Logo.svg" alt="background" />
   </button>
-</form>
+  <div class="text-header2 text-agray-700 mx-7 font-semibold font-primary mb-5">Log In</div>
+  <form>
+    <div class="mx-7 mb-2">
+      <label>
+        Email:
+        <input type="email" bind:value={email} />
+      </label>
+    </div>
+    <div class="mx-7 mb-64">
+      <label>
+        Password:
+        <input type="password" bind:value={password} />
+      </label>
+    </div>
+    <div class="flex flex-col items-center mb-5">
+      <button on:click={submit} class="text-header5 bg-primary w-11/12 text-center text-white py-3 font-semibold rounded-lg font-primary">
+        Log in
+      </button>
+    </div>
+  </form>
 
-<p>Welcome to Wally!</p>
-<p>Build your own personalized wallet buddy</p>
-<a href="/">Home</a>
+  <div class="mx-7 flex">
+    <div class="text-xs text-agray-500 font-semibold">Don't have an account yet?</div>
+    <a href="/signup" class="text-xs mx-1 text-primary font-semibold">Sign Up</a>
+  </div>
+</div>
