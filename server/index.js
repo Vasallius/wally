26ac3cc7c4d2bbc748e80b55a4064bd39eb0d0fb
smurfedGitHubIO <clydefrongoso@gmail.com>
2023-03-01 08:24:56 +0800

    Route testing

--- server/index.js
+++ server/index.js
@@ -1,21 +1,31 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const cors = require('cors');
-const ObjectId = require('mongodb').ObjectId;
 
 const app = express();
 
 const uri = process.env.uri;
 const client = new MongoClient(uri);
 
+const users = require('./routes/users');
+const wallets = require('./routes/wallets');
+const categories = require('./routes/categories');
+const subcategories = require('./routes/subcategories');
+const records = require('./routes/records');
+const dashboard_cards = require('./routes/dashboard_cards');
+
 app.use(express.json());
 app.use(cors());
+app.use('/users', users);
+app.use('/wallets', wallets);
+app.use('/categories', categories);
+app.use('/subcategories', subcategories);
+app.use('/records', records);
+app.use('/dashboard_cards', dashboard_cards);
 
 const main = async () => {
   try {
     await client.connect();
-
-
   } catch (error) {
     console.error(error);
   }
---
+++ server/routes/records.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const router = express.Router();
+
+const database = process.env.db;
+
+router.use((req, res, next) => {
+  next();
+});
+
+router.post('/create', async (req, res) => {
+  try {
+    const currentDate = new Date();
+
+    const body = {
+      balance: req.body.balance,
+      userID: req.body.userID,
+      category: req.body.category,
+      subcategory: req.body.subcategory,
+      dateissued: currentDate.getDate(),
+      timeissued: currentDate.getTime(),
+    };
+  
+    await client.db(database)
+                .collections("records")
+                .insertOne(body);
+    res.send(true);
+  } catch (error) {
+    console.error(error);
+    res.send(false);
+  }
+});
+
+router.post('/take', async (req, res) => {
+  try {
+    const filter = {
+      userID: req.body.userID,
+    };
+  
+    const results = await client.db(database)
+                                .collections("records")
+                                .find(filter)
+                                .toArray();
+    res.status(200).send(results);
+  } catch (error) {
+    console.error(error);
+    res.send([]);
+  }
+});
+
+router.delete('/delete/:id', async (req, res) => {
+  try {
+    const filter = {
+      _id: new ObjectId(req.params.id),
+    };
+
+    await client.db(database)
+                .collections("records")
+                .deleteOne(filter);
+    res.send(true);
+  } catch (error) {
+    console.error(error);
+    res.send(false);
+  }
+});
+
+module.exports = router;
