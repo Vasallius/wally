47bc7ae446c63430ad2af7b14ddedd8be243a8cc
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-03-03 16:07:37 +0800

    added records draft

---
+++ client/src/routes/records/+page.svelte
@@ -0,0 +1,58 @@
+<div class="flex flex-row mx-5 justify-between">
+    <img src="/X-icon.png" alt="">
+    <img src="/check.png" alt="">
+</div>
+<div class="flex flex-row ml-6 mr-4">
+    <div class= "py-3 px-6 text-header5 font-semibold bg-primary rounded-l-lg">Income</div>
+    <div class="py-3 px-6 text-header5 font-semibold bg-agray-300">Expense</div>
+    <div class="py-3 px-6 text-header5 font-semibold bg-agray-300 rounded-r-lg">Tracker</div>
+</div>
+<div class="flex flex-row">
+    <div>Account Wallet</div>
+    <div>Food and Drinks</div>
+</div>
+
+<div class="flex">
+    <img src="" alt="">
+    <div>2,500 PHP</div>
+</div>
+
+<div class="flex flex-row">
+    <div>C</div>
+    <div>()</div>
+    <div>%</div>
+    <div>/</div>
+
+</div>
+
+<div class="flex flex-row">
+    <div>C</div>
+    <div>()</div>
+    <div>%</div>
+    <div>/</div>
+
+</div>
+
+<div class="flex flex-row">
+    <div>C</div>
+    <div>()</div>
+    <div>%</div>
+    <div>/</div>
+
+</div>
+
+<div class="flex flex-row">
+    <div>C</div>
+    <div>()</div>
+    <div>%</div>
+    <div>/</div>
+
+</div>
+
+<div class="flex flex-row">
+    <div>C</div>
+    <div>()</div>
+    <div>%</div>
+    <div>/</div>
+
+</div>
