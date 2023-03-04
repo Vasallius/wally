62cb9f0afb3069bf703eff2d4edd7fa85e1ab9d7
Arian Rey <39580184+SwissNuff@users.noreply.github.com>
2023-03-05 00:27:03 +0800

    Refactor (client): settings

--- client/src/routes/(settings)/+layout.svelte
+++ client/src/routes/(settings)/+layout.svelte
@@ -2,7 +2,7 @@
 </script>
 
 <div
-	class="flex flex-col h-screen max-w-md justify-center m-auto border-solid border-2 border-sky-500"
+	class="flex flex-col h-screen max-w-md justify-start m-auto border-solid border-2 border-sky-500"
 >
 	<slot />
 </div>
