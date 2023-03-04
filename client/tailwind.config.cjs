45e9250f51c7004fc2c707700aeeda0f25d2c2fa
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-03-04 12:22:04 +0800

    added initial dashboard

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -3,21 +3,25 @@ module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {
-      fontSize:{
-        'header2':'24px',
-        'header5':'16px',
+      fontSize: {
+        'header2': '24px',
+        'header5': '16px',
         'small-arian': '12px',
       },
-      colors:{
+      colors: {
         'agray-700': '#3E3E3E',
         'agray-600': '#6A6A6A',
         'agray-300': '#F5F5F5',
         'agray-500': '#919191',
-        'primary' : '#00C09F',
+        'agray-50': '#FAFAFA',
+        'agray-200': '#F5F5F5',
+        'primary': '#00C09F',
+        'secondary': '#F12848',
+        'gdark': '#080808',
         'light-green': 'rgba(0, 192, 159, 0.15)'
       },
-      fontFamily:{
-        'primary':['Inter'],
+      fontFamily: {
+        'primary': ['Inter'],
       }
     }
   },
