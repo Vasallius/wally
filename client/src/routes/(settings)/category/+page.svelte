42ebdecfe6bb5d12ccabb59fd9b49701ef6828ae
Arian Rey <39580184+SwissNuff@users.noreply.github.com>
2023-03-07 22:17:24 +0800

    feat (client): Category record component and category page

---
+++ client/src/routes/(settings)/category/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import CategoryRecord from '../../../components/CategoryRecord.svelte';
+	import SettingsNav from '../../../components/SettingsNav.svelte';
+
+	export const name = 'wallet';
+</script>
+
+<SettingsNav>Category</SettingsNav>
+<CategoryRecord category="Shopping" />
