ba2f058e482c4e5f3095b9ad80f3467a0949684b
Arian Rey <39580184+SwissNuff@users.noreply.github.com>
2023-03-07 21:44:41 +0800

    feat (client): Notification record

---
+++ client/src/components/NotificationRecord.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	export let title: string;
+	export let content: string;
+	export let timePassed: number;
+	import { CalendarCheckFill } from 'svelte-bootstrap-icons';
+</script>
+
+<a href="/" class="flex flex-row px-4 py-4 justify-between border-b">
+	<div class="flex flex-row gap-4">
+		<div class="bg-light-green p-3 rounded-full">
+			<CalendarCheckFill fill="var(--primary)" width={20} height={20} />
+		</div>
+		<div class="flex flex-col">
+			<h1 class="text-base text-agray-700 font-bold">{title}</h1>
+			<p class="text-xs text-agray-500">{content}</p>
+		</div>
+	</div>
+	<div>
+		<p class="text-xs text-agray-500 font-semibold align-baseline">{timePassed} hr</p>
+	</div>
+</a>
+
+<style>
+	a:visited {
+		background: var(--agray-50);
+	}
+</style>
