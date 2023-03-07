78216a351d243a828101b0bf913bb2d0ca699871
Arian Rey <39580184+SwissNuff@users.noreply.github.com>
2023-03-07 21:45:20 +0800

    feat (client): notifications

---
+++ client/src/routes/(settings)/notification/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	import NotificationRecord from '../../../components/NotificationRecord.svelte';
+	import SettingsNav from '../../../components/SettingsNav.svelte';
+
+	export const name = 'wallet';
+</script>
+
+<SettingsNav>Notification</SettingsNav>
+<NotificationRecord
+	title="Reminder: Add today’s transaction"
+	content="Lorem Ipsum"
+	timePassed={1}
+/>
+<NotificationRecord
+	title="Reminder: Add today’s transaction"
+	content="Lorem Ipsum"
+	timePassed={2}
+/>
+<NotificationRecord
+	title="Reminder: Add today’s transaction"
+	content="Lorem Ipsum"
+	timePassed={3}
+/>
