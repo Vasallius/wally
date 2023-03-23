b6c43cdd939342498f86127102195184e310bd0e
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-03-23 17:12:21 +0800

    add stuff

--- client/src/routes/(app)/records/+page.svelte
+++ client/src/routes/(app)/records/+page.svelte
@@ -1,73 +1,63 @@
 <div class="flex flex-row w-11/12 mx-auto justify-between">
-    <img src="/X-icon.png" alt="">
-    <img src="/check.png" alt="">
+	<img src="/X-icon.png" alt="" />
+	<img src="/check.png" alt="" />
 </div>
 <div class="flex flex-row w-11/12 mx-auto">
-    <div class= "py-3 px-6 text-header5 font-semibold bg-primary rounded-l-lg">Income</div>
-    <div class="py-3 px-6 text-header5 font-semibold bg-agray-300">Expense</div>
-    <div class="py-3 px-6 text-header5 font-semibold bg-agray-300 rounded-r-lg">Tracker</div>
+	<div class="py-3 px-6 text-header5 font-semibold bg-primary rounded-l-lg">Income</div>
+	<div class="py-3 px-6 text-header5 font-semibold bg-agray-300">Expense</div>
+	<div class="py-3 px-6 text-header5 font-semibold bg-agray-300 rounded-r-lg">Tracker</div>
 </div>
 <div class="flex flex-row w-11/12 mb-16 mx-auto">
-    <div class="flex grow rounded-lg border-2 border-sky-500">
-        <img class="ml-4 mr-8" src="/WalletFill.svg" alt="background" />
-        <div class="flex flex-col">
-            <div class="text-super-sm font-primary font-semibold text-center ">Account</div>
-            <div class="text-sm font-primary font-bold">WALLET</div>
-        </div>
-    </div>
-    <div class="flex grow rounded-lg border-2 border-sky-500">
-        <img class="ml-4 mr-2" src="/TagFill.svg" alt="background" />
-        <div class="flex flex-col">
-            <div class="text-super-sm font-primary font-semibold text-center ">Category</div>
-            <div class="text-sm font-primary font-bold">Food & Drinks</div>
-        </div>
-    </div>
+	<div class="flex grow rounded-lg border-2 border-sky-500">
+		<img class="ml-4 mr-8" src="/WalletFill.svg" alt="background" />
+		<div class="flex flex-col">
+			<div class="text-super-sm font-primary font-semibold text-center ">Account</div>
+			<div class="text-sm font-primary font-bold">WALLET</div>
+		</div>
+	</div>
+	<div class="flex grow rounded-lg border-2 border-sky-500">
+		<img class="ml-4 mr-2" src="/TagFill.svg" alt="background" />
+		<div class="flex flex-col">
+			<div class="text-super-sm font-primary font-semibold text-center ">Category</div>
+			<div class="text-sm font-primary font-bold">Food & Drinks</div>
+		</div>
+	</div>
 </div>
 
 <div class="flex justify-between w-11/12 mx-auto">
-    <div class="border font-primary text-primary font-normal text-6xl self-end">+</div>
-    <div class="flex border">
-        <div class="border font-primary font-normal text-5xl self-end">2,500</div>
-        <div class="border self-end">PHP</div>
-    </div>
+	<div class="border font-primary text-primary font-normal text-6xl self-end">+</div>
+	<div class="flex border">
+		<div class="border font-primary font-normal text-5xl self-end">2,500</div>
+		<div class="border self-end">PHP</div>
+	</div>
 </div>
 
-<div class="flex flex-row">
-    <div class ="rounded-lg bg-agray-100 h-12 w-12 text-center">C</div>
-    <div class ="rounded-lg bg-agray-100 h-12 w-12 text-center">()</div>
-    <div class ="rounded-lg bg-agray-100 h-12 w-12 text-center">%</div>
-    <div class ="rounded-lg bg-agray-100 h-12 w-12 text-center">/</div>
+<div class="flex flex-col border w-11/12 mx-auto space-between gap-2">
+	<div class="flex flex-row grow justify-between h-[20vw] gap-2">
+		<button class="rounded-lg bg-agray-100 text-center grow border">C</button>
+		<button class="rounded-lg bg-agray-100 text-center grow ">()</button>
+		<button class="rounded-lg bg-agray-100  text-center grow ">%</button>
+		<button class="rounded-lg bg-agray-100  text-center grow ">÷</button>
+	</div>
 
-</div>
+	<div class="flex flex-row grow justify-between gap-2">
+		<button class="rounded-lg bg-agray-100 text-center grow border">C</button>
+		<button class="rounded-lg bg-agray-100 text-center grow ">()</button>
+		<button class="rounded-lg bg-agray-100  text-center grow ">%</button>
+		<button class="rounded-lg bg-agray-100  text-center grow ">÷</button>
+	</div>
 
-<div class="flex flex-row">
-    <div class ="rounded-lg bg-agray-100 h-12 w-12 text-center">7</div>
-    <div class ="rounded-lg bg-agray-100 h-12 w-12 text-center">()</div>
-    <div class ="rounded-lg bg-agray-100 h-12 w-12 text-center">%</div>
-    <div class ="rounded-lg bg-agray-100 h-12 w-12 text-center">/</div>
+    <div class="flex flex-row grow justify-between gap-2">
+		<button class="rounded-lg bg-agray-100 text-center grow border">C</button>
+		<button class="rounded-lg bg-agray-100 text-center grow ">()</button>
+		<button class="rounded-lg bg-agray-100  text-center grow ">%</button>
+		<button class="rounded-lg bg-agray-100  text-center grow ">÷</button>
+	</div>
 
-</div>
-
-<div class="flex flex-row">
-    <div class ="rounded-lg bg-agray-100 h-12 w-12 text-center">C</div>
-    <div class ="rounded-lg bg-agray-100 h-12 w-12 text-center">()</div>
-    <div class ="rounded-lg bg-agray-100 h-12 w-12 text-center">%</div>
-    <div class ="rounded-lg bg-agray-100 h-12 w-12 text-center">/</div>
-
-</div>
-
-<div class="flex flex-row">
-    <div class ="rounded-lg bg-agray-100 h-12 w-12 text-center">C</div>
-    <div class ="rounded-lg bg-agray-100 h-12 w-12 text-center">()</div>
-    <div class ="rounded-lg bg-agray-100 h-12 w-12 text-center">%</div>
-    <div class ="rounded-lg bg-agray-100 h-12 w-12 text-center">/</div>
-
-</div>
-
-<div class="flex flex-row">
-    <div class ="rounded-lg bg-agray-100 h-12 w-12 text-center">C</div>
-    <div class ="rounded-lg bg-agray-100 h-12 w-12 text-center">()</div>
-    <div class ="rounded-lg bg-agray-100 h-12 w-12 text-center">%</div>
-    <div class ="rounded-lg bg-agray-100 h-12 w-12 text-center">/</div>
-
+    <div class="flex flex-row grow justify-between gap-2">
+		<button class="rounded-lg bg-agray-100 text-center grow border">C</button>
+		<button class="rounded-lg bg-agray-100 text-center grow ">()</button>
+		<button class="rounded-lg bg-agray-100  text-center grow ">%</button>
+		<button class="rounded-lg bg-agray-100  text-center grow ">÷</button>
+	</div>
 </div>
