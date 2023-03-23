2242f220337d786f6a8ed21bfbf85f228738d250
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-03-23 14:06:13 +0800

    added work on records

--- client/src/routes/(app)/records/+page.svelte
+++ client/src/routes/(app)/records/+page.svelte
@@ -1,13 +1,13 @@
-<div class="flex flex-row mx-5 justify-between">
+<div class="flex flex-row w-11/12 mx-auto justify-between">
     <img src="/X-icon.png" alt="">
     <img src="/check.png" alt="">
 </div>
-<div class="flex flex-row ml-6 mr-4">
+<div class="flex flex-row w-11/12 mx-auto">
     <div class= "py-3 px-6 text-header5 font-semibold bg-primary rounded-l-lg">Income</div>
     <div class="py-3 px-6 text-header5 font-semibold bg-agray-300">Expense</div>
     <div class="py-3 px-6 text-header5 font-semibold bg-agray-300 rounded-r-lg">Tracker</div>
 </div>
-<div class="flex flex-row ml-6 mr-4">
+<div class="flex flex-row w-11/12 mb-16 mx-auto">
     <div class="flex grow rounded-lg border-2 border-sky-500">
         <img class="ml-4 mr-8" src="/WalletFill.svg" alt="background" />
         <div class="flex flex-col">
@@ -24,9 +24,12 @@
     </div>
 </div>
 
-<div class="flex">
-    <img src="" alt="">
-    <div>2,500 PHP</div>
+<div class="flex justify-between w-11/12 mx-auto">
+    <div class="border font-primary text-primary font-normal text-6xl self-end">+</div>
+    <div class="flex border">
+        <div class="border font-primary font-normal text-5xl self-end">2,500</div>
+        <div class="border self-end">PHP</div>
+    </div>
 </div>
 
 <div class="flex flex-row">
@@ -38,7 +41,7 @@
 </div>
 
 <div class="flex flex-row">
-    <div class ="rounded-lg bg-agray-100 h-12 w-12 text-center">C</div>
+    <div class ="rounded-lg bg-agray-100 h-12 w-12 text-center">7</div>
     <div class ="rounded-lg bg-agray-100 h-12 w-12 text-center">()</div>
     <div class ="rounded-lg bg-agray-100 h-12 w-12 text-center">%</div>
     <div class ="rounded-lg bg-agray-100 h-12 w-12 text-center">/</div>
