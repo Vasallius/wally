749b09e347098a3f6aa9479f1045d630fb802ec0
Arian Rey <39580184+SwissNuff@users.noreply.github.com>
2023-03-23 18:32:34 +0800

    refactor (client): budget

--- client/src/routes/(settings)/budget/+page.svelte
+++ client/src/routes/(settings)/budget/+page.svelte
@@ -5,8 +5,16 @@
 </script>
 
 <SettingsNav>Budget</SettingsNav>
-<div class= "flex flex-row justify-between bg-agray-50 text-agray-600 text-base font-semibold px-8 py-1"> Weekly </div>
-<BudgetRecord title="Transportation" balance={600} percent={0}/>
-<BudgetRecord title="Entertainment" balance={600} percent={89} />
-<div class= "flex flex-row justify-between bg-agray-50 text-agray-600 text-base font-semibold px-8 py-1"> Daily </div>
-<BudgetRecord title="Food" balance={600} percent={45}/>
+<div
+	class="flex flex-row justify-between bg-agray-50 text-agray-600 text-base font-semibold px-8 py-1"
+>
+	Weekly
+</div>
+<BudgetRecord title="Transportation" budgetSpent={200} budget={600} />
+<div
+	class="flex flex-row justify-between bg-agray-50 text-agray-600 text-base font-semibold px-8 py-1"
+>
+	Daily
+</div>
+<BudgetRecord title="Entertainment" budgetSpent={200} budget={800} />
+<BudgetRecord title="Food" budgetSpent={290} budget={300} />
