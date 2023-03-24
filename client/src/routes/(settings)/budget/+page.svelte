9b00f7723c0f29d2aae4d356b41ebe9915f7b7fd
miellekc <jccalderon@up.edu.ph>
2023-03-24 11:45:25 +0800

    added button

--- client/src/routes/(settings)/budget/+page.svelte
+++ client/src/routes/(settings)/budget/+page.svelte
@@ -10,3 +10,7 @@
 <BudgetRecord title="Entertainment" balance={600} percent={89} />
 <div class= "flex flex-row justify-between bg-agray-50 text-agray-600 text-base font-semibold px-8 py-1"> Daily </div>
 <BudgetRecord title="Food" balance={600} percent={45}/>
+
+<div class="flex flex-col mt-auto relative">
+	<button class="w-14 h-14 absolute bottom-8 right-8 rounded-full bg-primary text-3xl text-center text-white font-primary hover:opacity-90"> + </button>
+</div>
