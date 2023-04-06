f6921ec52bb4aeeed0f37bfa2818ab6079c4ec24
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-04-06 20:44:01 +0800

    feat(component): added two-way data binding to Textfield

--- client/src/components/Textfield.svelte
+++ client/src/components/Textfield.svelte
@@ -1,59 +1,60 @@
 <script lang="ts">
-    export let type: string;
-    export let id: string;
+	export let type: string;
+	export let id: string;
+	export let value: string;
 </script>
 
 <div class="text-field">
-    <input type={type} id={id} name={id} placeholder=" " required >
-    <label for={id}>
-        <slot></slot>
-    </label>
+	<input type="text" {id} name={id} placeholder=" " required bind:value />
+	<label for={id}>
+		<slot />
+	</label>
 </div>
 
 <style>
-.text-field {
-	position: relative;
-	margin: 1.2rem 0;
-}
+	.text-field {
+		position: relative;
+		margin: 1.2rem 0;
+	}
 
-.text-field input {
-    width: 100%;
-	border: 1.5px solid gray;
-	-webkit-transition: 0.5s ;
-    transition: 0.5s ;
-	outline: none ;
-	padding: 12px 20px 10px 10px ;
-	border-radius: 5px ;
-}
+	.text-field input {
+		width: 100%;
+		border: 1.5px solid gray;
+		-webkit-transition: 0.5s;
+		transition: 0.5s;
+		outline: none;
+		padding: 12px 20px 10px 10px;
+		border-radius: 5px;
+	}
 
-.text-field input:hover {
-	border: 1.5px solid black ;
-}
+	.text-field input:hover {
+		border: 1.5px solid black;
+	}
 
-.text-field input:focus {
-	border: 1.5px solid var(--primary) ;
-}
+	.text-field input:focus {
+		border: 1.5px solid var(--primary);
+	}
 
-.text-field label {
-	content: '';
-	position: absolute;
-	top: 14px;
-	left: 10px;
-	color: gray;
-	font-size: 14px;
-	pointer-events: none;
-	transition: 0.2s ease all; 
-}
+	.text-field label {
+		content: '';
+		position: absolute;
+		top: 14px;
+		left: 10px;
+		color: gray;
+		font-size: 14px;
+		pointer-events: none;
+		transition: 0.2s ease all;
+	}
 
-.text-field input:focus ~ label, .text-field input:not(:placeholder-shown) ~ label {
-	font-size: 11px;
-	top: -9px;
-	transition: 0.2s ease all; 
-    -moz-transition: 0.2s ease all; 
-    -webkit-transition: 0.2s ease all;
-	color: var(--primary);
-	background-color: white;
-	padding: 2px;
-}
-
+	.text-field input:focus ~ label,
+	.text-field input:not(:placeholder-shown) ~ label {
+		font-size: 11px;
+		top: -9px;
+		transition: 0.2s ease all;
+		-moz-transition: 0.2s ease all;
+		-webkit-transition: 0.2s ease all;
+		color: var(--primary);
+		background-color: white;
+		padding: 2px;
+	}
 </style>
--- client/src/routes/(auth)/login/+page.svelte
+++ client/src/routes/(auth)/login/+page.svelte
@@ -1,43 +1,78 @@
 <script>
-	import Textfield from '../../../components/Textfield.svelte';
+	let loggedIn = false;
+	let username = 'test';
+	let password = 'test';
+	const clicked = async () => {
+		await fetch('http://127.0.0.1:3002/listdatabases', {
+			method: 'GET',
+			headers: {
+				'Content-type': 'application/json'
+			}
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				data.forEach((/** @type {string} */ val) => {
+					console.log(val);
+				});
+			});
+	};
 
-	let username = '';
-	let fullname = '';
-	let email = '';
-	let password = '';
+	const submit = async () => {
+		let body = { username, password };
+		console.log(body);
+		await fetch('http://127.0.0.1:3002/login', {
+			method: 'POST',
+			headers: {
+				'Content-type': 'application/json'
+			},
+			body: JSON.stringify(body)
+		})
+			.then((res) => res.json())
+			.then((dat) => {
+				if (dat !== null) {
+					loggedIn = !loggedIn;
+					localStorage.setItem('loggedIn', 'true');
+					localStorage.setItem('userID', dat);
+				}
+			});
+	};
 
+	const logout = () => {
+		loggedIn = !loggedIn;
+		localStorage.setItem('loggedIn', 'false');
+		localStorage.removeItem('loggedIn');
+		localStorage.removeItem('userID');
+	};
 </script>
 
-<div>
-	<a href="/">
-		<div class="mx-7 w-12 h-12 bg-light-green mb-8 rounded-full">
-			<img
-				class="w-12 h-12 transition ease-in-out hover:-translate-x-0.5 duration-100"
-				src="/ArrowLeftShort.svg"
-				alt="background"
-			/>
-		</div>
-	</a>
-	<div class="text-header2 text-agray-700 mx-7 font-semibold font-primary mb-5">Log In</div>
-	<form method="POST">
-		<div class="mx-7 mb-2">
-			<Textfield type="email" id="email">Email</Textfield>
-		</div>
-		<div class="mx-7 mb-64">
-			<Textfield type="password" id="password">Password</Textfield>
-		</div>
-		<div class="flex flex-col items-center mb-5">
-			<button
-				type="submit"
-				class="text-header5 bg-primary w-11/12 text-center text-white py-3 font-semibold rounded-lg font-primary hover:opacity-90"
+<div
+	class="flex flex-col h-screen max-w-md justify-center m-auto border-solid border-2 border-sky-500"
+>
+	<div class="mx-auto w-36 h-36 bg-light-green mb-36 rounded-full">
+		<img class="w-36 h-36" src="/Logo.svg" alt="background" />
+	</div>
+	<div class="text-header2 text-agray-700 mx-7 font-semibold font-primary mb-5">
+		Welcome to Wally!
+	</div>
+	<div class="text-xs text-agray-600 mx-7 mb-5 font-normal font-primary" />
+	<div>
+		<div class="flex flex-col items-center mb-9 mx-7">
+			<a
+				href="/signup"
+				class="text-header5 bg-primary w-full text-center text-white py-3 font-semibold rounded-lg font-primary hover:opacity-90"
 			>
-				Log in
-			</button>
+				Sign up with email
+			</a>
+			<div class="w-11/12 text-xs text-agray-500 width-screen my-2 text-center">or</div>
+			<a
+				href="/"
+				class="text-header5 text-agray-600 bg-agray-300 w-full text-center py-3 font-semibold rounded-lg font-primary hover:opacity-80"
+				>Continue with Google</a
+			>
 		</div>
-	</form>
-
+	</div>
 	<div class="mx-7 flex">
-		<div class="text-xs text-agray-500 font-semibold">Don't have an account yet?</div>
-		<a href="/signup" class="text-xs mx-1 text-primary font-semibold hover:underline">Sign Up</a>
+		<div class="text-xs text-agray-500 font-semibold">Already have an account?</div>
+		<a href="/login" class="text-xs mx-1 text-primary font-semibold hover:underline">Log In</a>
 	</div>
 </div>
