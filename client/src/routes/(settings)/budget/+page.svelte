5107c18f3151c3d1eab0dbf9b88be1c343a725ce
Jaimielle Kyle Calderon <jccalderon@up.edu.ph>
2023-04-16 06:05:26 +0800

    refactor: budget

--- client/src/routes/(settings)/budget/+page.svelte
+++ client/src/routes/(settings)/budget/+page.svelte
@@ -17,11 +17,19 @@
 </script>
 
 <SettingsNav>Budget</SettingsNav>
-<div class= "flex flex-row justify-between bg-agray-50 text-agray-600 text-base font-semibold px-8 py-1"> Weekly </div>
-<BudgetRecord title="Transportation" balance={600} percent={0}/>
-<BudgetRecord title="Entertainment" balance={600} percent={89} />
-<div class= "flex flex-row justify-between bg-agray-50 text-agray-600 text-base font-semibold px-8 py-1"> Daily </div>
-<BudgetRecord title="Food" balance={600} percent={45}/>
+<div
+	class="flex flex-row justify-between bg-agray-50 text-agray-600 text-base font-semibold px-8 py-1"
+>
+	Weekly
+</div>
+<BudgetRecord title="Transportation" budgetSpent={200} budget={600} />
+<div
+	class="flex flex-row justify-between bg-agray-50 text-agray-600 text-base font-semibold px-8 py-1"
+>
+	Daily
+</div>
+<BudgetRecord title="Entertainment" budgetSpent={200} budget={800} />
+<BudgetRecord title="Food" budgetSpent={290} budget={300} />
 
 <div class="flex flex-col mt-auto relative">
 	<button on:click={openPopUp} class="w-14 h-14 absolute bottom-8 right-8 rounded-full bg-primary text-3xl text-center text-white font-primary hover:opacity-90"> + </button>
