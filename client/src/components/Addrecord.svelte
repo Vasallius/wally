fc03be955a8e7ff895eb0e23abbae1ea13656e60
Arian Rey <39580184+SwissNuff@users.noreply.github.com>
2023-04-25 00:13:03 +0800

    fix (feat): calculator

--- client/src/components/Addrecord.svelte
+++ client/src/components/Addrecord.svelte
@@ -1,12 +1,18 @@
-<script>
-	import { fly } from 'svelte/transition';
-
+<!-- <script>
 	let numberInput = '';
 	let total = 0;
 
-	// private function
 	function addToEquation(value) {
-		numberInput += value;
+		if (value === 'backspace') {
+			numberInput = numberInput.slice(0, -1);
+		} else if (value === '.') {
+			// prevent adding multiple decimal points
+			if (numberInput.slice(-1) !== '.') {
+				numberInput += '.';
+			}
+		} else {
+			numberInput += value;
+		}
 	}
 
 	const clear = () => {
@@ -16,22 +22,23 @@
 
 	function calculate() {
 		if (numberInput !== '') {
-			numberInput = result().toString();
+			const resultValue = result();
+			const roundedResult = resultValue.toFixed(2);
+			const formattedResult = parseFloat(roundedResult).toString();
+			numberInput = formattedResult;
 		}
-	}
-
-	function replaceAll(string, search, replace) {
-		return string.split(search).join(replace);
-	}
-
-	function formatString(value) {
-		return replaceAll(replaceAll(value, '*', 'x'), '/', '÷');
 	}
 
 	// computed
 	let result = () => {
 		if (!isNaN(numberInput.slice(-1))) {
-			return eval(numberInput);
+			let expr = numberInput;
+			if (expr.includes('%')) {
+				let exprParts = expr.split('%');
+				let percentValue = parseFloat(exprParts[0]) / 100;
+				expr = percentValue.toString() + exprParts.slice(1).join('');
+			}
+			return eval(expr);
 		}
 		return eval(numberInput.slice(0, -1));
 	};
@@ -42,80 +49,91 @@
 	}
 </script>
 
-<section class="app">
-	<div class="results">
-		<div class="calculations">{numberInput}</div>
-		{#if total !== 0}
-			<input transition:fly={{ x: 10, duration: 800 }} type="text" class="total" value={total} />
-		{/if}
+<div>
+	<div class="flex flex-row w-11/12 mx-auto">
+		<div class="py-3 px-6 text-header5 font-semibold bg-primary rounded-l-lg">Income</div>
+		<div class="py-3 px-6 text-header5 font-semibold bg-agray-300">Expense</div>
+		<div class="py-3 px-6 text-header5 font-semibold bg-agray-300 rounded-r-lg">Tracker</div>
+	</div>
+	<div class="flex flex-row w-11/12 mb-16 mx-auto">
+		<div class="flex grow rounded-lg border-2 border-sky-500">
+			<img class="ml-4 mr-8" src="/WalletFill.svg" alt="background" />
+			<div class="flex flex-col">
+				<div class="text-super-sm font-primary font-semibold text-center ">Account</div>
+				<div class="text-sm font-primary font-bold">WALLET</div>
+			</div>
+		</div>
+		<div class="flex grow rounded-lg border-2 border-sky-500">
+			<img class="ml-4 mr-2" src="/TagFill.svg" alt="background" />
+			<div class="flex flex-col">
+				<div class="text-super-sm font-primary font-semibold text-center ">Category</div>
+				<div class="text-sm font-primary font-bold">Food & Drinks</div>
+			</div>
+		</div>
 	</div>
 
-	<div class="input-pad">
-		<button expand="3" type="clear" clicked={clear} />
-		<button type="operator" clicked={() => addToEquation('/')}>÷</button>
-
-		<button clicked={() => addToEquation(9)}>9</button>
-		<button clicked={() => addToEquation(8)}>8</button>
-		<button clicked={() => addToEquation(7)}>7</button>
-
-		<button type="operator" clicked={() => addToEquation('*')}>x</button>
-		<button clicked={() => addToEquation(6)}>6</button>
-		<button clicked={() => addToEquation(5)}>5</button>
-		<button clicked={() => addToEquation(4)}>4</button>
-
-		<button type="operator" clicked={() => addToEquation('-')}>-</button>
-		<button clicked={() => addToEquation(3)}>3</button>
-		<button clicked={() => addToEquation(2)}>2</button>
-		<button clicked={() => addToEquation(1)}>1</button>
-
-		<button type="operator" clicked={() => addToEquation('+')}>+</button>
-		<button expand="3" clicked={() => addToEquation(0)}>0</button>
-
-		<button type="equal" clicked={() => calculate()}>=</button>
+	<div class="flex justify-between w-11/12 mx-auto border-b-2 pb-6">
+		<div class="font-primary text-primary font-normal text-6xl">+</div>
+		<div class="flex">
+			<input
+				class="font-primary font-normal text-5xl self-end focus:outline-none text-right w-full "
+				bind:value={numberInput}
+				onblur="this.focus()"
+				autofocus
+				on:keydown={(event) => {
+					if (event.key === 'Enter') {
+						calculate();
+					}
+				}}
+			/>
+			<div class="self-end p-[6px]">PHP</div>
+		</div>
 	</div>
-</section>
+
+	<div class="keypad w-11/12 mx-auto py-6">
+		<button on:click={clear}>C</button>
+		<button on:click={() => addToEquation('**')}>x<sup>□</sup></button>
+		<button on:click={() => addToEquation('%')}>%</button>
+		<button on:click={() => addToEquation('/')}>÷</button>
+
+		<button on:click={() => addToEquation(7)}>7</button>
+		<button on:click={() => addToEquation(8)}>8</button>
+		<button on:click={() => addToEquation(9)}>9</button>
+		<button on:click={() => addToEquation('*')}>x</button>
+
+		<button on:click={() => addToEquation(4)}>4</button>
+		<button on:click={() => addToEquation(5)}>5</button>
+		<button on:click={() => addToEquation(6)}>6</button>
+		<button on:click={() => addToEquation('-')}>-</button>
+
+		<button on:click={() => addToEquation(1)}>1</button>
+		<button on:click={() => addToEquation(2)}>2</button>
+		<button on:click={() => addToEquation(3)}>3</button>
+		<button on:click={() => addToEquation('+')}>+</button>
+
+		<button on:click={() => addToEquation('.')}>.</button>
+		<button on:click={() => addToEquation(0)}>0</button>
+		<button on:click={() => addToEquation('backspace')}>backspace</button>
+		<button on:click={() => calculate()}>=</button>
+	</div>
+</div>
 
 <style>
-	:global(body) {
-		color: #333;
-		margin: 0;
-		padding: 20px;
-		box-sizing: border-box;
-		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu,
-			Cantarell, 'Helvetica Neue', sans-serif;
-	}
-
-	.app {
-		max-width: 300px;
-	}
-
-	.input-pad {
+	.keypad {
 		display: grid;
 		grid-template-columns: repeat(4, 25%);
 		grid-template-rows: repeat(5, 20%);
-		grid-column-gap: 0;
-		grid-row-gap: 0;
 	}
 
-	.results {
-		display: flex;
-		height: 94px;
-		flex-direction: column;
-		text-align: right;
+	button {
+		grid-column: span 1;
+		padding: 1.3rem 1rem;
+		background-color: var(--agray-100);
+		border-radius: 10px;
+		margin: 5px;
+		font-size: 16px;
 	}
-
-	.calculations {
-		height: 20px;
-		color: #828282;
-		padding: 0 10px;
+	button:active {
+		background-color: var(--agray-400) !important;
 	}
-
-	.total {
-		color: #333;
-		text-align: right;
-		padding: 10px;
-		font-size: 44px;
-		margin: 0;
-		border: none;
-	}
-</style>
+</style> -->
