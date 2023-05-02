d298d3a40b56609de4d6817c0e8d4414eb7335a5
smurfedGitHubIO <clydefrongoso@gmail.com>
2023-05-02 20:42:21 +0800

    feat: server apis

--- client/src/routes/(app)/dashboard/+page.svelte
+++ client/src/routes/(app)/dashboard/+page.svelte
@@ -1,72 +1,105 @@
 <script>
-	import { Bell } from 'svelte-bootstrap-icons';
+	// @ts-nocheck
+	import { auth, logOut } from '../../../server/routes/usersAPI';
+	import { onMount } from 'svelte';
+	import { onAuthStateChanged } from 'firebase/auth';
 	import RecordCard from '../../../components/RecordCard.svelte';
+	let val = [];
+	let user = null;
+
+	onAuthStateChanged(auth, (currentUser) => {
+		console.log('state changed');
+		user = currentUser;
+		console.log('Current user:', user);
+	});
+
+	const isAuthenticated = () => {
+		return user !== null;
+	};
+	console.log(auth);
+
+	function handleLogout() {
+		const success = logOut();
+		if (success) {
+			// Redirect to login page or update UI as needed
+		} else {
+			// Handle error
+		}
+	}
 </script>
 
-<div class="flex flex-col mx-auto w-11/12">
-	<div class="flex flex-row my-3 justify-between">
-		<div class="flex">
-			<a href="/menu"><div class="w-5 h-5 my-auto bg-light-green rounded-full mr-5" /></a>
-			<div class="font-primary font-semibold text-header5 text-black">Dashboard</div>
+{#if user}
+	<div>{user.auth.currentUser.email}</div>
+	<button on:click={handleLogout}>Logout</button>
+	<div>
+		<div class="flex mx-3 mt-4 db-nav">
+			<div class="flex justify-center items-center">
+				<div class="w-4 h-4 my-auto bg-light-green rounded-full mr-5" />
+			</div>
+			<div class="font-primary font-semibold mr-56 text-header5 text-black">Dashboard</div>
+			<div class="flex justify-center items-center">
+				<img class="w-4 h-4 my-auto" src="/Bell.svg" alt="background" />
+			</div>
 		</div>
-		<div class="flex items-center">
-			<a href="/notification"><Bell fill="gray" /></a>
-		</div>
-	</div>
-	<div class="db-summary">
-		<div class="flex flex-col bg-agray-50 rounded-xl border border-agray-200 border-solid">
-			<div class="mx-5 my-3 flex flex-row justify-between items-center ">
-				<div class="font-primary text-header5 text-agray-700 font-semibold">February 28, 2022</div>
+		<div class="db-summary mx-3">
+			<div class="flex flex-col bg-agray-50 rounded-xl border border-agray-200 border-solid">
+				<div class="mx-5 my-3 flex flex-row justify-between items-center ">
+					<div class="font-primary text-header5 text-agray-700 font-semibold">
+						February 28, 2022
+					</div>
 
-				<img class="w-4 h-4 my-auto" src="/ThreeDots.svg" alt="background" />
-			</div>
-			<div class="mx-5  mb-2 flex flex-row justify-between items-center ">
-				<div class="font-primary  font-normal  text-gdark text-xs">Total Income</div>
-				<div class="font-primary  font-semibold text-primary text-xs">₱2,500</div>
-			</div>
-			<div
-				class="mx-5  flex flex-row justify-between items-center pb-1.5 border-b border-sky-500 mb-1.5"
-			>
-				<div class="font-primary  font-normal  text-agray-700 text-xs">Total Expenses</div>
-				<div class="font-primary  font-semibold text-secondary text-xs">-₱1,500</div>
-			</div>
-			<div class="mx-5 flex flex-row justify-between items-center pb-4">
-				<div class="font-primary  font-normal  text-agray-700 text-xs">Total</div>
-				<div class="font-primary  font-semibold text-agray-700 text-xs">₱1,000</div>
+					<img class="w-4 h-4 my-auto" src="/ThreeDots.svg" alt="background" />
+				</div>
+				<div class="mx-5  mb-2 flex flex-row justify-between items-center ">
+					<div class="font-primary  font-normal  text-gdark text-xs">Total Income</div>
+					<div class="font-primary  font-semibold text-primary text-xs">₱{val[0]}</div>
+				</div>
+				<div
+					class="mx-5  flex flex-row justify-between items-center pb-1.5 border-b border-sky-500 mb-1.5"
+				>
+					<div class="font-primary  font-normal  text-agray-700 text-xs">Total Expenses</div>
+					<div class="font-primary  font-semibold text-secondary text-xs">-₱{val[1]}</div>
+				</div>
+				<div class="mx-5  flex flex-row justify-between items-center pb-4">
+					<div class="font-primary  font-normal  text-agray-700 text-xs">Total</div>
+					<div class="font-primary  font-semibold text-agray-700 text-xs">₱{val[0] - val[1]}</div>
+				</div>
 			</div>
 		</div>
-	</div>
-	<div class="flex mt-3.5 mb-1.5">
-		<div class="text-header5 font-primary font-semibold">Wallets</div>
-	</div>
-	<div class="flex carousel">
-		<div class="flex flex-col pl-2 pt-3 mr-3 grow h-16 bg-primary rounded-lg">
-			<div class="text-agray-200 font-medium text-xs font-primary">CASH</div>
-			<div class="text-agray-200 font-primary font-semibold text-header5">₱2,500</div>
+		<div class="flex mx-3 mt-3.5 mb-1.5">
+			<div class="text-header5 font-primary font-semibold">Wallets</div>
 		</div>
+		<div class="flex carousel mx-3">
+			<div class="flex flex-col pl-2 pt-3 mr-3 grow h-16 bg-primary rounded-lg">
+				<div class="text-agray-200 font-medium text-xs font-primary">CASH</div>
+				<div class="text-agray-200 font-primary font-semibold text-header5">₱2,500</div>
+			</div>
 
-		<div class="flex flex-col pl-2 pt-3 mr-3 grow h-16 bg-primary rounded-lg">
-			<div class="text-agray-200 font-medium text-xs font-primary">BANK</div>
-			<div class="text-agray-200 font-primary font-semibold text-header5">₱2,500</div>
-		</div>
+			<div class="flex flex-col pl-2 pt-3 mr-3 grow h-16 bg-primary rounded-lg">
+				<div class="text-agray-200 font-medium text-xs font-primary">BANK</div>
+				<div class="text-agray-200 font-primary font-semibold text-header5">₱2,500</div>
+			</div>
 
-		<div class="flex flex-col pl-2 pt-3 grow -16 bg-primary rounded-lg">
-			<div class="text-agray-200 font-medium text-xs font-primary">CASH</div>
-			<div class="text-agray-200 font-primary font-semibold text-header5">₱2,500</div>
+			<div class="flex flex-col pl-2 pt-3 grow -16 bg-primary rounded-lg">
+				<div class="text-agray-200 font-medium text-xs font-primary">CASH</div>
+				<div class="text-agray-200 font-primary font-semibold text-header5">₱2,500</div>
+			</div>
+		</div>
+		<div class="flex mx-3 mt-5 mb-4">
+			<div class="text-header5 font-primary font-semibold">Records</div>
+		</div>
+		<div class="flex mx-3 space-x-1.5">
+			<div class="bg-agray-200 text-xs py-2.5 rounded-lg font-medium px-3">All</div>
+			<div class="bg-agray-200 text-xs py-2.5 rounded-lg px-3">Income</div>
+			<div class="bg-agray-200 text-xs py-2.5 rounded-lg px-3">Expense</div>
+			<div class="bg-agray-200 text-xs py-2.5 rounded-lg px-3">Transfer</div>
+		</div>
+		<div class="flex flex-col">
+			<RecordCard category="Foods & Drinks" wallet="Cash" />
+			<RecordCard category="Income" wallet="Bank" />
+			<RecordCard category="Foods & Drinks" wallet="Gcash" />
 		</div>
 	</div>
-	<div class="flex mt-5 mb-4">
-		<div class="text-header5 font-primary font-semibold">Records</div>
-	</div>
-	<div class="flex space-x-1.5">
-		<div class="bg-agray-200 text-xs py-2.5 rounded-lg font-medium px-3">All</div>
-		<div class="bg-agray-200 text-xs py-2.5 rounded-lg px-3">Income</div>
-		<div class="bg-agray-200 text-xs py-2.5 rounded-lg px-3">Expense</div>
-		<div class="bg-agray-200 text-xs py-2.5 rounded-lg px-3">Transfer</div>
-	</div>
-	<div class="flex flex-col">
-		<RecordCard category="Foods & Drinks" wallet="Cash" />
-		<RecordCard category="Income" wallet="Bank" />
-		<RecordCard category="Foods & Drinks" wallet="Gcash" />
-	</div>
-</div>
+{:else}
+	<div>You must be authenticated to access the dashboard.</div>
+{/if}
--- client/src/routes/(auth)/login/+page.svelte
+++ client/src/routes/(auth)/login/+page.svelte
@@ -1,86 +1,68 @@
-<script lang="ts">
+<script>
+// @ts-nocheck
+
 	import Textfield from '../../../components/Textfield.svelte';
-	import {
-		getAuth,
-		onAuthStateChanged,
-		type User,
-		signInWithEmailAndPassword
-	} from 'firebase/auth';
-	import { initializeApp } from 'firebase/app';
-	import { onMount } from 'svelte';
+	import { logIn } from '../../../server';
+
+	// @ts-ignore
 	let username = '';
+	// @ts-ignore
 	let fullname = '';
+	// @ts-ignore
 	let email = '';
+	// @ts-ignore
 	let password = '';
-	let user: User | null;
 
-	const firebaseConfig = {
-		apiKey: 'AIzaSyDTLyLUaVgvIihRMDLOXypjqfBenGh2fQk',
-		authDomain: 'wally-75d4e.firebaseapp.com',
-		projectId: 'wally-75d4e',
-		storageBucket: 'wally-75d4e.appspot.com',
-		messagingSenderId: '518053974573',
-		appId: '1:518053974573:web:8aa5410f6c4b98ffd75ea4'
+	const login = async () => {
+			try {
+					// @ts-ignore
+					let x = document.querySelector('.login').email.value;
+					// @ts-ignore
+					let y = document.querySelector('.login').password.value;
+					console.log(x, y);
+
+					const errorMessage = await logIn(x, y); // Add 'await' here
+					if (errorMessage === '') {
+							console.log('goods');
+					} else {
+							console.log('error:', errorMessage);
+					}
+			} catch (error) {
+					console.log('error');
+			}
 	};
-	const app = initializeApp(firebaseConfig);
-
-	const login = () => {
-		console.log('test');
-		console.log('email and pass');
-		console.log(email, password);
-		const auth = getAuth(app);
-
-		signInWithEmailAndPassword(auth, email, password)
-			.then((userCredential) => {
-				// Signed in
-				const user = userCredential.user;
-				console.log(user);
-				// ...
-			})
-			.catch((error) => {
-				const errorCode = error.code;
-				const errorMessage = error.message;
-			});
-	};
-
-	onMount(async () => {
-		const auth = getAuth(app);
-		onAuthStateChanged(auth, (newUser) => {
-			user = newUser;
-		});
-	});
 </script>
 
 <div>
 	<a href="/">
-		<div class="mx-7 w-12 h-12 bg-light-green mb-8 rounded-full">
-			<img
-				class="w-12 h-12 transition ease-in-out hover:-translate-x-0.5 duration-100"
-				src="/ArrowLeftShort.svg"
-				alt="background"
-			/>
-		</div>
+			<div class="mx-7 w-12 h-12 bg-light-green mb-8 rounded-full">
+					<img
+							class="w-12 h-12 transition ease-in-out hover:-translate-x-0.5 duration-100"
+							src="/ArrowLeftShort.svg"
+							alt="background"
+					/>
+			</div>
 	</a>
 	<div class="text-header2 text-agray-700 mx-7 font-semibold font-primary mb-5">Log In</div>
-	<form>
-		<div class="mx-7 mb-2">
-			<Textfield bind:value={email} type="email" id="email">Email</Textfield>
-		</div>
-		<div class="mx-7 mb-64">
-			<Textfield bind:value={password} type="password" id="password">Password</Textfield>
-		</div>
-		<div class="flex flex-col items-center mb-5">
-			<button
-				on:click={login}
-				class="text-header5 bg-primary w-11/12 text-center text-white py-3 font-semibold rounded-lg font-primary hover:opacity-90"
-			>
-				Log in
-			</button>
-		</div>
+	<form class="login">
+			<div class="mx-7 mb-2">
+					<Textfield type="email" id="email">Email</Textfield>
+			</div>
+			<div class="mx-7 mb-64">
+					<Textfield type="password" id="password">Password</Textfield>
+			</div>
+			<div class="flex flex-col items-center mb-5">
+					<button
+							on:click={login}
+							class="text-header5 bg-primary w-11/12 text-center text-white py-3 font-semibold rounded-lg font-primary hover:opacity-90"
+					>
+							Log in
+					</button>
+			</div>
 	</form>
 
 	<div class="mx-7 flex">
-		<div class="text-xs text-agray-500 font-semibold">Don't have an account yet?</div>
-		<a href="/signup" class="text-xs mx-1 text-primary font-semibold hover:underline">Sign Up</a>
+			<div class="text-xs text-agray-500 font-semibold">Don't have an account yet?</div>
+			<a href="/signup" class="text-xs mx-1 text-primary font-semibold hover:underline">Sign Up</a>
 	</div>
 </div>
