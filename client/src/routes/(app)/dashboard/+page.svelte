7f64dc384b6c3ece6beda64754fe5b15419fe87f
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-05-04 09:26:55 +0800

    feat: Create RecordBar component  Create a new `RecordBar` component that holds the records and allows the user to filter them by category. The component consists of a header that says "Records" and a set of buttons that allow the user to filter the records by category (All, Income, Expense, Transfer).  This change improves the organization and modularity of the app, making it easier to reuse and maintain the code. It also adds a new feature that enhances the user experience and allows the user to view and manage the records more efficiently.  Note that this change does not affect the functionality of the app, but it does improve its overall quality and usability.

--- client/src/routes/(app)/dashboard/+page.svelte
+++ client/src/routes/(app)/dashboard/+page.svelte
@@ -7,6 +7,8 @@
 	import RecordCard from '../../../components/RecordCard.svelte';
 	import { getMonthlySummary } from '../../../server';
 	import Wallet from '../../../components/Wallet.svelte';
+	import { Record } from 'svelte-bootstrap-icons';
+	import RecordBar from '../../../components/RecordBar.svelte';
 	let val = [0, 0];
 	let user = null;
 
@@ -73,15 +75,7 @@
 			</div>
 		</div>
 		<Wallet/>
-		<div class="flex mx-3 mt-5 mb-4">
-			<div class="text-header5 font-primary font-semibold">Records</div>
-		</div>
-		<div class="flex mx-3 space-x-1.5">
-			<div class="bg-agray-200 text-xs py-2.5 rounded-lg font-medium px-3">All</div>
-			<div class="bg-agray-200 text-xs py-2.5 rounded-lg px-3">Income</div>
-			<div class="bg-agray-200 text-xs py-2.5 rounded-lg px-3">Expense</div>
-			<div class="bg-agray-200 text-xs py-2.5 rounded-lg px-3">Transfer</div>
-		</div>
+		<RecordBar/>
 		<div class="flex flex-col">
 			<RecordCard category="Foods & Drinks" wallet="Cash" />
 			<RecordCard category="Income" wallet="Bank" />
