2d4c2fcc9d76e6c7d49bbcf17fad9b1b940230d7
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-05-05 21:10:03 +0800

    feat: update wallet component

--- client/src/components/Wallet.svelte
+++ client/src/components/Wallet.svelte
@@ -8,24 +8,24 @@
 	let wallets = [];
 	export let user;
 
-	{
-		(async () => {
-			if (user) {
-				// monthlySummary = await getMonthlySummary(user.uid).then((val) => val);
-				// records = await getDashboardRecords(user.uid).then((val) => val);
-				wallets = await getWallets(user.uid).then((val) => val);
-				// console.log(monthlySummary, records, wallets);
-			} else {
-			}
-		})();
+	// Retrieve the user's wallets from the server
+	async function fetchWallets() {
+		return getWallets(user.uid);
 	}
+
+	// Set the `promise` variable to the result of calling `fetchWallets()`
+	let promise = fetchWallets();
 </script>
 
 <div class="flex mx-3 mt-3.5 mb-1.5">
 	<div class="text-header5 font-primary font-semibold">Wallets</div>
 </div>
 <div class="flex carousel mx-3">
-	{#each wallets as wallet}
-		<WalletItem label={wallet.name} amount={wallet.balance} active={wallet.active} />
-	{/each}
+	{#await promise then wallets}
+		{#each wallets as wallet}
+			<WalletItem label={wallet.name} amount={wallet.balance} active={wallet.active} />
+		{/each}
+	{:catch error}
+		<div class="error">Error: {error.message}</div>
+	{/await}
 </div>
