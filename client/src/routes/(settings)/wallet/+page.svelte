aba8f2458e6710cb62bbb4936e6bc0c1b940e2f8
Arian Rey <39580184+SwissNuff@users.noreply.github.com>
2023-05-06 23:38:04 +0800

    refactor (client): components for wallet, budget, categories

--- client/src/routes/(settings)/wallet/+page.svelte
+++ client/src/routes/(settings)/wallet/+page.svelte
@@ -2,37 +2,37 @@
 	import SettingsNav from '../../../components/SettingsNav.svelte';
 	import WalletRecord from '../../../components/WalletRecord.svelte';
 	import PopUpWallet from '../../../components/PopUpWallet.svelte';
-	
+
 	export const name = 'wallet';
 	let isModalOpen = false;
-  	let label = '';
-  	let amount = 0;
+	let label = '';
+	let amount = 0;
 	let WalletRecords = [
-		{ id: 0, component: WalletRecord, title:"Cash", balance: 600 },
-		{ id: 1, component: WalletRecord, title:"Maya", balance: 1200 },
-		{ id: 2, component: WalletRecord, title:"Bank (BPI)", balance: 20000 },
+		{ id: 0, title: 'Cash', balance: 600 },
+		{ id: 1, title: 'Maya', balance: 1200 },
+		{ id: 2, title: 'Bank (BPI)', balance: 20000 }
 	];
 
-  	const openPopUp = () => {
-    	isModalOpen = true;
-  	};
-		
+	const openPopUp = () => {
+		isModalOpen = true;
+	};
 </script>
 
 <SettingsNav>Wallet</SettingsNav>
 
 {#each WalletRecords as item}
-	<svelte:component this={item.component} title={item.title} balance={item.balance}/>
+	<WalletRecord title={item.title} balance={item.balance} />
 {/each}
 
 <div class="flex flex-col mt-auto relative">
-	<button on:click={openPopUp} class="w-14 h-14 absolute bottom-8 right-8 rounded-full bg-primary text-3xl text-center text-white font-primary hover:opacity-90 pb-1"> + </button>
+	<button
+		on:click={openPopUp}
+		class="w-14 h-14 absolute bottom-8 right-8 rounded-full bg-primary text-3xl text-center text-white font-primary hover:opacity-90 pb-1"
+	>
+		+
+	</button>
 </div>
 
 <div class="absolute z-50 h-full m-auto ">
-	<PopUpWallet
-		bind:isOpen={isModalOpen}
-		label={label}
-		amount={amount}
-	/>
+	<PopUpWallet bind:isOpen={isModalOpen} {label} {amount} />
 </div>
