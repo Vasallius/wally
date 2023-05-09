9e746703ad3611b45bd6bab1cdefb454213b89ae
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-05-09 14:12:30 +0800

    feat: update route back to menu

--- client/src/components/SettingsNav.svelte
+++ client/src/components/SettingsNav.svelte
@@ -2,7 +2,7 @@
 </script>
 
 <div class="bg-primary flex flex-row gap-1">
-	<a href="/dashboard" class="m-3">
+	<a href="/menu" class="m-3">
 		<img class="w-10 h-10" src="/ArrowLeftShortWhite.svg" alt="background" />
 	</a>
 	<h1 class="text-white text-xl font-semibold my-auto"><slot /></h1>
