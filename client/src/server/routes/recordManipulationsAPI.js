73bbdfcbe617a6035099113c83e817529da62bf5
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-05-09 17:38:59 +0800

    refactor: add spaces

--- client/src/server/routes/recordManipulationsAPI.js
+++ client/src/server/routes/recordManipulationsAPI.js
@@ -1,7 +1,7 @@
 // @ts-nocheck
 /* eslint-disable no-unused-vars */
 import { collection, getDocs, addDoc, deleteDoc, onSnapshot, doc, updateDoc, query, where } from 'firebase/firestore'
-import { db, auth} from './firebase'
+import { db, auth } from './firebase'
 
 
 export const addRecord = async (userID, record) => {
@@ -10,20 +10,20 @@ export const addRecord = async (userID, record) => {
     where('userID', '==', userID)
   );
   const records = await getDocsUtility(recordsReference)
-  .then((val) => {
-    return val;
-  });
+    .then((val) => {
+      return val;
+    });
   record["date"] = new Date();
   records[0].records.push(record);
   const documentReference = doc(db, 'records', records[0].id);
-  updateDoc(documentReference, {records: records[0].records});
+  updateDoc(documentReference, { records: records[0].records });
 }
 
 const getDocsUtility = async (collectionReference) => {
   let records = []
   const querySnap = await getDocs(collectionReference);
   querySnap.forEach((doc) => {
-    records.push({...doc.data(), id: doc.id});
+    records.push({ ...doc.data(), id: doc.id });
   });
   return records;
 };
@@ -47,14 +47,14 @@ export const getRecord = async (userID, index) => {
   const recordsReference = query(collectionReference, where('userID', '==', userID));
   let record = [];
   const querySnap = await getDocs(recordsReference)
-  .then((val) => {
-    val.forEach((doc) => {
-      record.push({...doc.data(), id: doc.id});
-    })
-  });
-  if(index >= record[0].records.length){
+    .then((val) => {
+      val.forEach((doc) => {
+        record.push({ ...doc.data(), id: doc.id });
+      })
+    });
+  if (index >= record[0].records.length) {
     return null;
-  } else{
+  } else {
     return record[0].records[index];
   }
 }
@@ -64,18 +64,18 @@ export const deleteRecord = async (userID, index) => {
   const recordsReference = query(collectionReference, where('userID', '==', userID));
   let record = [];
   const querySnap = await getDocs(recordsReference)
-  .then((val) => {
-    val.forEach((doc) => {
-      record.push({...doc.data(), id: doc.id});
-    })
-  });
-  if(index < record[0].records.length){
+    .then((val) => {
+      val.forEach((doc) => {
+        record.push({ ...doc.data(), id: doc.id });
+      })
+    });
+  if (index < record[0].records.length) {
     record[0].records = record[0].records.filter((val, ind) => {
       return ind != index;
     })
     const documentReference = doc(db, 'records', record[0].id)
-  
-    updateDoc(documentReference, {records: record[0].records});
+
+    updateDoc(documentReference, { records: record[0].records });
     return true;
   } else {
     return false;
@@ -87,24 +87,24 @@ export const editRecord = async (userID, index, updateValues) => {
   const recordsReference = query(collectionReference, where('userID', '==', userID));
   let record = [];
   const querySnap = await getDocs(recordsReference)
-  .then((val) => {
-    val.forEach((doc) => {
-      record.push({...doc.data(), id: doc.id});
-    })
-  });
-  if(index < record[0].records.length){
-    for(const [key, values] of Object.entries(record[0].records[index])){
-      for(const [key2, values2] of Object.entries(updateValues)){
-        if(key == key2){
+    .then((val) => {
+      val.forEach((doc) => {
+        record.push({ ...doc.data(), id: doc.id });
+      })
+    });
+  if (index < record[0].records.length) {
+    for (const [key, values] of Object.entries(record[0].records[index])) {
+      for (const [key2, values2] of Object.entries(updateValues)) {
+        if (key == key2) {
           record[0].records[index][key] = values2;
           break;
         }
       }
     }
-    
+
     const documentReference = doc(db, 'records', record[0].id)
-  
-    updateDoc(documentReference, {records: record[0].records});
+
+    updateDoc(documentReference, { records: record[0].records });
     return true;
   } else {
     return false;
