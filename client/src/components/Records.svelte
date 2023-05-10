ce4e92c94ef55b6851cd228eaeb0b269c6a3081a
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-05-10 22:38:29 +0800

    refactor: removed code

--- client/src/components/Records.svelte
+++ client/src/components/Records.svelte
@@ -4,23 +4,7 @@
 	import { recordsStore, authStore } from '../server/stores/stores';
 	import { onMount } from 'svelte';
 
-	// @ts-ignore
 	export let user;
-
-	const months = [
-		'Jan',
-		'Feb',
-		'Mar',
-		'Apr',
-		'May',
-		'Jun',
-		'Jul',
-		'Aug',
-		'Sep',
-		'Oct',
-		'Nov',
-		'Dec'
-	];
 
 	onMount(async () => {
 		// Fetch the data from the database
@@ -30,18 +14,10 @@
 		recordsStore.set(initialData);
 	});
 
-	async function fetchDashboardRecords() {
-		// @ts-ignore
-		return getDashboardRecords(user.uid, 'Cash');
-	}
-
-	// @ts-ignore
-	let promise = fetchDashboardRecords();
 	function convertTimestamp(timestamp) {
 		const date = new Date(timestamp.seconds * 1000);
 		return date.toLocaleDateString('en-US', { month: 'short', day: 'numeric' });
 	}
-	console.log(promise);
 </script>
 
 {#if $recordsStore}
