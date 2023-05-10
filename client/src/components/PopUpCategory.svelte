8a6a78f8bd23a4d601fec139ff992b9b10d42c3d
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-05-10 22:27:16 +0800

    refactor: removed unnecessary import

--- client/src/components/PopUpCategory.svelte
+++ client/src/components/PopUpCategory.svelte
@@ -1,36 +1,42 @@
 <script>
-    import CategoryRecord from "./CategoryRecord.svelte";
-    import CategoryRecords from "../routes/(settings)/category/+page.svelte"
-    export let isOpen = false;
-    export let label = '';
-  
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      console.log(`Label: ${label}`);
-      closeModal();
-    };
-  
-    const closeModal = () => {
-      isOpen = false;
-      label = '';
-    };
-  </script>
+	export let isOpen = false;
+	export let label = '';
 
-  {#if isOpen}
-    <div class="flex w-full min-h-full items-center bg-black/40 px-20 border border-x-1 border-black/40 border-r-transparent">
-      <form on:submit={handleSubmit} 
-        class="flex flex-col gap-6 bg-agray-50 rounded-xl text-agray-700 text-base font-semibold pt-8 pb-6 px-8 mx-2.5">
-        <div class="flex w-auto flex-col gap-6">
-          <div class="relative h-10 w-48 min-w-[200px]">
-            <input
-              class="peer h-full w-full rounded-[7px] border border-agray-600 border-t-transparent bg-transparent px-3 py-2.5 font-primary text-sm 
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		console.log(`Label: ${label}`);
+		closeModal();
+	};
+
+	const closeModal = () => {
+		isOpen = false;
+		label = '';
+	};
+</script>
+
+{#if isOpen}
+	<div
+		class="flex w-full min-h-full items-center bg-black/40 px-20 border border-x-1 border-black/40 border-r-transparent"
+	>
+		<form
+			on:submit={handleSubmit}
+			class="flex flex-col gap-6 bg-agray-50 rounded-xl text-agray-700 text-base font-semibold pt-8 pb-6 px-8 mx-2.5"
+		>
+			<div class="flex w-auto flex-col gap-6">
+				<div class="relative h-10 w-48 min-w-[200px]">
+					<input
+						class="peer h-full w-full rounded-[7px] border border-agray-600 border-t-transparent bg-transparent px-3 py-2.5 font-primary text-sm 
               font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-agray-600 
               placeholder-shown:border-t-agray-600 focus:border-2 focus:border-teal-500 focus:border-t-transparent focus:outline-0 
               disabled:border-0 disabled:bg-blue-gray-50"
-              placeholder=" " type="text" id="label-input" bind:value={label}
-            />
-            <label for="label-input" 
-              class="before:content[' '] after:content[' '] font-normal pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] 
+						placeholder=" "
+						type="text"
+						id="label-input"
+						bind:value={label}
+					/>
+					<label
+						for="label-input"
+						class="before:content[' '] after:content[' '] font-normal pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] 
               font-primary leading-tight text-agray-700 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border 
               before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-agray-600 
               before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 
@@ -40,20 +46,25 @@
               peer-focus:leading-tight peer-focus:text-teal-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 
               peer-focus:before:border-teal-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-teal-500 
               peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent 
-              peer-disabled:peer-placeholder-shown:text-blue-gray-500"> 
-              Label
-            </label>
-          </div>
-              <!-- can add choices for icon --> 
-        </div>
-        <div class="relative self-center flex flex-row gap-x-12 font-normal pr-1">
-          <div class="pb-0.5 w-16 relative text-center text-white bg-primary rounded-full shadow-lg shadow-inherit hover:opacity-70">
-              <button type="submit">save</button>
-          </div>
-          <div class="w-16 relative text-center text-white rounded-full bg-agray-500 shadow-lg shadow-inherit hover:opacity-70"> 
-            <button on:click={closeModal}>close</button>
-          </div>
-        </div>
-      </form>
-    </div>
-  {/if}
+              peer-disabled:peer-placeholder-shown:text-blue-gray-500"
+					>
+						Label
+					</label>
+				</div>
+				<!-- can add choices for icon -->
+			</div>
+			<div class="relative self-center flex flex-row gap-x-12 font-normal pr-1">
+				<div
+					class="pb-0.5 w-16 relative text-center text-white bg-primary rounded-full shadow-lg shadow-inherit hover:opacity-70"
+				>
+					<button type="submit">save</button>
+				</div>
+				<div
+					class="w-16 relative text-center text-white rounded-full bg-agray-500 shadow-lg shadow-inherit hover:opacity-70"
+				>
+					<button on:click={closeModal}>close</button>
+				</div>
+			</div>
+		</form>
+	</div>
+{/if}
