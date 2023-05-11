ad78e1fd67635f1fc33c48907c48992f7fbf1d51
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-05-11 22:45:47 +0800

    feat: added new walletStores

--- src/server/stores/stores.js
+++ src/server/stores/stores.js
@@ -2,23 +2,6 @@
 // stores.js
 import { writable } from 'svelte/store';
 export const authStore = writable(null);
-function createWalletStore() {
-    const { subscribe, set, update } = writable([]);
-
-    return {
-        subscribe,
-        set,
-        update,
-        walletAdded: () => update(wallets => {
-            // Emit a custom event when a wallet is added
-            const event = new CustomEvent('walletAdded');
-            document.dispatchEvent(event);
-            return wallets;
-        })
-    };
-}
-
 export const recordsStore = writable(null)
 export const categoriesStore = writable(null)
-
-export const walletStore = createWalletStore();
+export const walletStores = writable(null)
