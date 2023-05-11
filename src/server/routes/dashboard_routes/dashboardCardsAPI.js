dc0ff7a304b93c957f822514c8a40723ee3de410
Justin Clyde V. Frongoso <clydefrongoso@gmail.com>
2023-05-11 21:41:30 +0800

    chore: sort records by date

--- src/server/routes/dashboard_routes/dashboardCardsAPI.js
+++ src/server/routes/dashboard_routes/dashboardCardsAPI.js
@@ -115,7 +115,10 @@ export const getDashboardRecords = async (userID, currentActiveWallet) => {
   const docRef = doc(db, 'records', userID)
   const docSnap = await getDoc(docRef)
   if (docSnap.exists()) {
-      const data = docSnap.data().records;
+      const data = docSnap.data().records.sort((first, second) => {
+        // console.log(secondDate, firstDate);
+        return Number(new Date(second.date.seconds*1000+second.date.nanoseconds)) - Number(new Date(first.date.seconds*1000+first.date.nanoseconds));
+      });
       return data
     } else {
       return "NO WALLETS"
