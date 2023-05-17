622b8e1508e2928fa7d6fa6318a276c73d9009bf
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-05-17 22:48:01 +0800

    feat: ✨ fix budget redirection issue

--- src/components/BudgetRecord.svelte
+++ src/components/BudgetRecord.svelte
@@ -5,7 +5,7 @@
 	$: percent = (budgetSpent / budget) * 100;
 </script>
 
-<a href="/" class="flex flex-col px-8 py-4 justify-between border-b hover:bg-agray-50">
+<div class="flex flex-col px-8 py-4 justify-between border-b hover:bg-agray-50">
 	<div class="flex flex-row justify-between">
 		<h1 class="text-base text-agray-700 font-semibold pb-1">{title}</h1>
 		<p class="text-xs text-agray-600">₱{budget}</p>
@@ -13,4 +13,4 @@
 	<div class="flex w-full bg-gray-200 rounded-full h-2">
 		<div class="flex bg-accent-green h-2 rounded-full" style="width: {percent}%" />
 	</div>
-</a>
+</div>
