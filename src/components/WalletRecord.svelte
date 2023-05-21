6053999b8c84d121b1de652f93c01d88730923e1
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-05-21 15:56:49 +0800

    feat(wallet): ✨ add delete wallet function #36

--- src/components/WalletRecord.svelte
+++ src/components/WalletRecord.svelte
@@ -1,24 +1,24 @@
 <script lang="ts">
 	// @ts-nocheck
+	import { Trash } from 'svelte-bootstrap-icons';
+	import { walletStores } from '../server/stores/stores';
 
 	export let title: string;
 	export let balance: number;
-	export let options = '';
+
+	function handleDeleteClick() {
+		let wallets = $walletStores;
+		wallets = wallets.filter((wallet) => wallet.name != title);
+		walletStores.set(wallets);
+	}
 </script>
 
-<div class="flex flex-row px-8 py-4 justify-between border-b hover:bg-agray-50">
+<div class="flex flex-row px-8 py-4 justify-between border-b hover:bg-agray-50 items-center">
 	<div class="flex flex-col">
 		<h1 class="text-base text-agray-700 font-semibold">{title}</h1>
 		<p class="text-xs text-agray-600">₱{balance}</p>
 	</div>
-
-	<select
-		id="options"
-		bind:value={options}
-		class="focus:outline-none text-agray-700 bg-transparent border-transparent w-6 font-primary text-sm rounded"
-	>
-		<option value="" />
-		<option value="Delete">Delete</option>
-		<option value="Edit">Edit</option>
-	</select>
+	<button on:click={handleDeleteClick}>
+		<Trash fill="#00C09F" />
+	</button>
 </div>
