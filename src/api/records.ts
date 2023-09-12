de9ac83fb99021ca6c364e49c28a5337a739c7e7
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-09-13 01:08:57 +0800

    refactor: add getallrecords

---
+++ src/api/records.ts
@@ -0,0 +1,24 @@
+import { Timestamp, doc, getDoc } from 'firebase/firestore';
+import { db } from '../server';
+
+interface Record {
+	amount: number;
+	category: string;
+	date: Timestamp;
+	name: string;
+	recordType: string;
+	wallet: string;
+	wallet2: string;
+}
+export const getAllRecords = async (userID: string) => {
+	const docRef = doc(db, 'records', userID);
+	const docSnap = await getDoc(docRef);
+
+	if (docSnap.exists()) {
+		const data = docSnap.data().records;
+		return data;
+	} else {
+		console.log('No such document!');
+		return [];
+	}
+};
