62b3cc1a80230877bfdba4d86d1d6d9bcc1a39df
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-09-13 00:40:20 +0800

    refactor: add alias

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,8 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		alias: {
-			$components: 'src/components'
+			$components: 'src/components',
+			$api: 'src/api'
 		}
 	},
 	preprocess: vitePreprocess()
