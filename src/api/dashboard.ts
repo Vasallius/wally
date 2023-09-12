f90ca51c6f65dc30552d8b1a43a92a66053acd37
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-09-13 00:40:35 +0800

    refactor: update function to ts

--- src/api/dashboard.ts
+++ src/api/dashboard.ts
@@ -1,5 +1,13 @@
 import { db } from '$lib/firebase/firebase';
-import { doc, getDoc } from 'firebase/firestore';
+import { Timestamp, doc, getDoc } from 'firebase/firestore';
+
+/**
+ * A function that returns the list of wallets for the
+ * specified user.
+ * @param  userID
+ * @returns A list that contains the wallets for the
+ * specified user.
+ */
 export const getWallets = async (userID: string) => {
 	const docRef = doc(db, 'wallets', userID);
 	const docSnap = await getDoc(docRef);
@@ -11,3 +19,68 @@ export const getWallets = async (userID: string) => {
 		return 'NO WALLETS';
 	}
 };
+
+interface Record {
+	amount: number;
+	category: string;
+	date: Timestamp;
+	name: string;
+	recordType: string;
+	wallet: string;
+}
+
+// /**
+//  * a function that gets all the records for a specific
+//  * wallet of a user.
+//  * @param {string} recordtype
+//  * @param {string} userid
+//  * @param {string} currentactivewallet
+//  * @returns a filtered list that contains all records
+//  * of a specific record and wallet.
+//  */
+const getRecords = async (recordType: string, userID: string, currentActiveWallet: string) => {
+	const docRef = doc(db, 'records', userID);
+	const docSnap = await getDoc(docRef);
+	const data = docSnap.data();
+	if (data) {
+		return data.records.filter((currentRecord: Record) => {
+			return currentRecord.recordType == recordType && currentRecord.wallet == currentActiveWallet;
+		});
+	} else {
+		return 'NO RECORDS';
+	}
+};
+
+/**
+ * This function sums the amounts of the records that match the provided record type and wallet name.
+ *
+ * @param {Record[]} array - The array of records to be processed.
+ * @param {string} recordType - The type of record to be included in the sum.
+ * @param {string} walletName - The wallet name of the records to be included in the sum.
+ * @returns {number} The total sum of the amounts of the matching records.
+ */
+export const sumRecords = function (array: Record[], recordType: string, walletName: string) {
+	return array.reduce((total: number, current: Record) => {
+		if (current.recordType === recordType && current.wallet === walletName) {
+			return total + current.amount;
+		} else {
+			return total;
+		}
+	}, 0);
+};
+
+// export const sumTransferFrom = function (array, walletname) {
+// 	let transfer = array.filter(
+// 		(wallet) => wallet.recordType == 'transfer' && wallet.wallet == walletname
+// 	);
+// 	let sum = transfer.reduce((accumulator, currentValue) => accumulator + currentValue.amount, 0);
+// 	return sum;
+// };
+
+// export const sumTransferTo = function (array, walletname) {
+// 	let transfer = array.filter(
+// 		(wallet) => wallet.recordType == 'transfer' && wallet.wallet2 == walletname
+// 	);
+// 	let sum = transfer.reduce((accumulator, currentValue) => accumulator + currentValue.amount, 0);
+// 	return sum;
+// };
