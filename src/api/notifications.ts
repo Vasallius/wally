ecbeb59b9f45fdc6e18fa325782d238d55c17f17
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-09-13 11:20:14 +0800

    refactor to typescript

--- src/api/notifications.ts
+++ src/api/notifications.ts
@@ -1,4 +1,8 @@
 import { getBudgets } from '$api/budgets';
+interface Notif {
+	title: string;
+	content: string;
+}
 /**
  * Creates a list of notifications.
  * @param {string} userID
@@ -8,7 +12,7 @@ export const notificationsList = async (userID: string) => {
 	const budgetsList = await getBudgets(userID);
 	const keys = ['DayRecords', 'WeekRecords', 'MonthRecords'];
 	const interval = ['Daily', 'Weekly', 'Monthly'];
-	const res = [];
+	const notifs: Notif[] = [];
 	for (let j = 0; j < 3; j++) {
 		for (let i = 0; i < budgetsList[keys[j]].length; i++) {
 			if (budgetsList[keys[j]][i].budget != 0) {
@@ -16,17 +20,17 @@ export const notificationsList = async (userID: string) => {
 					budgetsList[keys[j]][i].spent >= budgetsList[keys[j]][i].budget * 0.75 &&
 					budgetsList[keys[j]][i].spent < budgetsList[keys[j]][i].budget
 				) {
-					res.push({
+					notifs.push({
 						title: `[${budgetsList[keys[j]][i].title}] ${interval[j]} Budget Limit`,
 						content: 'Your money spent is nearing the budget limit.'
 					});
 				} else if (budgetsList[keys[j]][i].spent == budgetsList[keys[j]][i].budget) {
-					res.push({
+					notifs.push({
 						title: `[${budgetsList[keys[j]][i].title}] ${interval[j]} Budget Reached  `,
 						content: `You have already spent your total budget.`
 					});
 				} else {
-					res.push({
+					notifs.push({
 						title: `[${budgetsList[keys[j]][i].title}] ${interval[j]} Budget Exceeded  `,
 						content: `You have spent ₱${budgetsList[keys[j]][i].spent} which is over your budget ₱${
 							budgetsList[keys[j]][i].budget
@@ -36,5 +40,5 @@ export const notificationsList = async (userID: string) => {
 			}
 		}
 	}
-	return res;
+	return notifs;
 };
